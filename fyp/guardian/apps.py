7af51bd14808439e4d035c44e56da5868a068b77
Rishi Raj Shrestha <np05cp4a190126@iic.edu.np>
2021-12-04 16:32:55 +0545

    set up

--- fyp/guardian/apps.py
+++ fyp/guardian/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GuardainConfig(AppConfig):
+class GuardianConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'guardain'
+    name = 'guardian'

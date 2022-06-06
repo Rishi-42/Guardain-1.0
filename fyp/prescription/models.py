acc8b3ea722392b172678470a2613fa626230049
Rishi Raj Shrestha <np05cp4a190126@iic.edu.np>
2022-06-06 21:54:08 +0545

    removed data

--- fyp/prescription/models.py
+++ fyp/prescription/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class UploadPrescription(models.Model):
     name = models.CharField(max_length=35)
     age = models.PositiveIntegerField()
-    prescription = models.FileField(upload_to='prescription/')
+    prescription = models.FileField(upload_to='prescription_img/')
     prescripted_by = models.CharField(max_length=35)
     prescripted_for = models.CharField(max_length=50)
 
